44841ea4b6f6d393a4e6bcbe3991ba11d284f58f
JasonRoyalHart <jason@jasonrhart.com>
2016-12-12 11:26:44 -0600

    First commit.

---
+++ CustomList/CustomBuiltList.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    class CustomBuiltList<T>
+    {
+        T[] elements;
+        public CustomBuiltList()
+        {
+   
+        }
+        public int Count()
+        {
+            int count = 0;
+            if (elements != null)
+            {
+                foreach (object elem in elements)
+                {
+                    count++;
+                }
+                return count;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public void Add(T toAdd)
+        {
+            int count = 0;
+            T[] newElements = new T[Count() + 1];
+            if (elements != null)
+            {
+                foreach (T elem in elements)
+                {
+                    newElements[count] = elem;
+                    count++;
+                }
+                newElements[count] = toAdd;
+            }
+            else
+            {
+                newElements[0] = toAdd;
+            }
+            elements = newElements;
+        }
+
+        public bool Remove(T toRemove)
+        {
+            T[] newElements = new T[Count() - 1];
+            int count = 0;
+            bool found = false;
+            int foundIndex = 0;
+            foreach (T element in elements)
+            {
+                if (element.Equals(toRemove))
+                {
+                    found = true;
+                    foundIndex = count;
+                }
+                count++;
+            }
+            if (found)
+            {
+                count = 0;
+                int newCount = 0;
+                foreach (T element in elements)
+                {
+                    if (count != foundIndex)
+                    {
+                        newElements[newCount] = element;
+                        count++;
+                        newCount++;
+                    }
+                    else
+                    {
+                        count++;
+                    }
+                }
+                elements = newElements;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("{0} was not found in CustomList.",toRemove);
+                return false;
+            }
+        }
+        public override string ToString()
+        {
+            string convertedString = "";
+            foreach (T element in elements)
+            {
+                convertedString += element;
+            }
+            return convertedString;
+        }
+        public static CustomBuiltList<T> operator +(CustomBuiltList<T> customListOne, CustomBuiltList<T> customListTwo)
+        {
+            CustomBuiltList<T> mergedCustomBuiltList = new CustomBuiltList<T>();
+            foreach (T element in customListOne.elements)
+            {
+                mergedCustomBuiltList.Add(element);
+            }
+            foreach (T element in customListTwo.elements)
+            {
+                mergedCustomBuiltList.Add(element);
+            }
+            return mergedCustomBuiltList;
+        }
+        public void Display()
+        {
+            if (elements != null)
+            {
+                foreach (object elem in elements)
+                {
+                    Console.WriteLine(elem);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Null list.");
+            }
+        }
+    }
+}
---
+++ CustomList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomBuiltList<string> testList = new CustomBuiltList<string>();
+            Console.WriteLine(testList.Count());
+            testList.Add("String 1");
+            testList.Add("String 2");
+            testList.Add("String 3");
+            testList.Display();
+            testList.Remove("String 3");
+            testList.Display();
+            //List<string> realListOfStrings = new List<String>();
+            //realListOfStrings.Add("Hello");
+            //Console.WriteLine(realListOfStrings.ToString());
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
